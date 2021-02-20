32ecf68c2024227d4917a15fe561c519cc1f7540
Andreas Olsson <noname.olsson@gmail.com>
2021-02-20 10:49:00 +0100

    refactor: 💡 Remove non-existing eslint rule

--- src/logo/styled.d.ts
+++ src/logo/styled.d.ts
@@ -1,7 +1,6 @@
 import { TLogoTheme } from './logo.theme'
 
 declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/interface-name-prefix
   export interface DefaultTheme {
     Logo: TLogoTheme
   }
